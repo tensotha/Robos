f58b05c8f9c6d86fccbfa19f3df461b5caf73808
Abdulhakim Alsharif <abalsh@ttu.ee>
2019-07-02 12:54:51 +0300

    Add Structure to SearchBox

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
 const SearchBox = () => {
-
+    return (
+        <div className='pa2'>
+            <input 
+            className = 'pa3 ba b--blue bg-lightest-blue'
+            type='search' 
+            placeholder='search robots'/>
+        </div>
+        
+    )
 }
+export default SearchBox;
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react';
 import CardList from '../components/CardList';
 import {robots} from '../robots';
+import SearchBox from '../components/SearchBox';
 const App = () => {
     return (
-        <div>
+        <div className='tc'>
+            <SearchBox />
             <h1>RoboUsers</h1>
             <CardList robots={robots}/>
         </div>
