d1b6ff017fa0b7c3f2ea1883915fa7ba95d06523
Abdulhakim Alsharif <abalsh@ttu.ee>
2019-07-02 12:30:27 +0300

    Destructure Props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'tachyons';
 
-const Card = (props) =>{
+const Card = ({name, email, id}) =>{
     return(
         <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow5">
-            <img alt="robots" src={`https://robohash.org/${props.id}?200x200`}/>
+            <img alt="robots" src={`https://robohash.org/${id}?200x200`}/>
             <div>
-                <h2>{props.name}</h2>
-                <p>{props.email}</p>
+                <h2>{name}</h2>
+                <p>{email}</p>
             </div>
         </div>
     );
