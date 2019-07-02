affb59ec92328575bec375d422722feeba53c55b
Abdulhakim Alsharif <abalsh@ttu.ee>
2019-07-02 12:50:01 +0300

    Structure Project

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react';
-import Card from '../components/Card';
 import CardList from '../components/CardList';
-import './App.css';
-import 'tachyons';
-
-ReactDOM.render(<Card />, document.getElementById('root'));
-export default;
+import {robots} from '../robots';
+const App = () => {
+    return (
+        <div>
+            <h1>RoboUsers</h1>
+            <CardList robots={robots}/>
+        </div>
+    )
+}
+export default App;
