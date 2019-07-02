5fbfd286966df0e63068d98a6cf92e29fc3c4bf4
Abdulhakim Alsharif <abalsh@ttu.ee>
2019-07-02 15:07:33 +0300

    Implement Provider with Store

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,6 +1,6 @@
 import { CHANGE_SEARCHFIELD } from "./constants";
 
-export const setSearcgField = (text) => ({
+export const setSearchField = (text) => ({
     type: CHANGE_SEARCHFIELD,
     payload: text
 })
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,7 +1,13 @@
+import {CHANGE_SEARCHFIELD} from './constants';
 const initialState = {
     searchField: ''
 }
 
 export const searchRobots  = (state=initialState, action={}) => {
-
+    switch(action.type){
+        case CHANGE_SEARCHFIELD:
+            return Object.assign({}, state, {searchField: action.payload}) // Another way: { ...state, searchField: action.payload}
+        default:
+            return state;
+    }
 }
