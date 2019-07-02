25fb17de8ffbf863e6896d3dbe9d58d71ba4f3ea
Abdulhakim Alsharif <abalsh@ttu.ee>
2019-07-02 13:00:40 +0300

    Convert App from Const to Class

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react';
 import CardList from '../components/CardList';
 import {robots} from '../robots';
 import SearchBox from '../components/SearchBox';
-const App = () => {
-    return (
-        <div className='tc'>
-            <SearchBox />
-            <h1>RoboUsers</h1>
-            <CardList robots={robots}/>
-        </div>
-    )
+
+class App extends React.Component{
+    render(){
+        return (
+            <div className='tc'>
+                <SearchBox />
+                <h1>RoboUsers</h1>
+                <CardList robots={robots}/>
+            </div>
+        )
+    }
+
 }
 export default App;
