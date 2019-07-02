164eb6cc2f68a283b222c642f00dbf9af5536f33
Abdulhakim Alsharif <abalsh@ttu.ee>
2019-07-02 12:28:15 +0300

    Implement Props to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'tachyons';
 
-const Card = () =>{
+const Card = (props) =>{
     return(
-        <div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow5">
-            <img src='https://robohash.org/robos?200x200' alt="robots"/>
+        <div className="tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow5">
+            <img alt="robots" src={`https://robohash.org/${props.id}?200x200`}/>
             <div>
-                <h2>Jane Doe</h2>
-                <p>jane.doe@gmail.com</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './components/Card';
 import * as serviceWorker from './serviceWorker';
+import {robots} from './robots';
 
 ReactDOM.render(
     <div>
-    <Card/>
-    <Card />
-    <Card />
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
     </div>
     , document.getElementById('root'));
 
