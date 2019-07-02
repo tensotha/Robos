60c39612f309f8fcf14481d26652a4d616b19ea7
Abdulhakim Alsharif <abalsh@ttu.ee>
2019-07-02 12:36:40 +0300

    Implement CardList

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './components/Card';
+import CardList from './components/CardList';
 import * as serviceWorker from './serviceWorker';
 import {robots} from './robots';
 
 ReactDOM.render(
     <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+        <CardList robots={ robots } />
     </div>
     , document.getElementById('root'));
 
